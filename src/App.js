d927f4dc6a32b7a0d48bf7e5f880f36502506638
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-05-17 15:19:49 -0300

    [add] App wrapped with context providers

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Login from "./pages/login/login";
 import Events from "./pages/events/events";
 import Bookings from "./pages/bookings/bookings";
 import MainNav from "./components/navigation/mainNav";
+import AuthContextProvider from "./context/authContext";
+import InterfaceContextProvider from "./context/interfaceContext";
 
 import "antd/dist/antd.css";
 
@@ -15,35 +17,39 @@ const { Content, Footer, Sider } = Layout;
 function App() {
   return (
     <BrowserRouter>
-      <Layout className="layout">
-        <MainNav />
-        <Layout>
-          <Sider>Sider</Sider>
-          <Layout style={{ padding: "0 24px 24px" }}>
-            <Content
-              style={{
-                padding: 24,
-                margin: 0,
-                minHeight: 280,
-              }}
-            >
-              <Switch>
-                <Redirect from="/" to="/auth" exact></Redirect>
-                <Route path="/auth">
-                  <Login />
-                </Route>
-                <Route path="/events">
-                  <Events />
-                </Route>
-                <Route path="/bookings">
-                  <Bookings />
-                </Route>
-              </Switch>
-            </Content>
+      <AuthContextProvider>
+        <InterfaceContextProvider>
+          <Layout className="layout">
+            <MainNav />
+            <Layout>
+              <Sider>Sider</Sider>
+              <Layout style={{ padding: "0 24px 24px" }}>
+                <Content
+                  style={{
+                    padding: 24,
+                    margin: 0,
+                    minHeight: 280,
+                  }}
+                >
+                  <Switch>
+                    <Redirect from="/" to="/auth" exact></Redirect>
+                    <Route path="/auth">
+                      <Login />
+                    </Route>
+                    <Route path="/events">
+                      <Events />
+                    </Route>
+                    <Route path="/bookings">
+                      <Bookings />
+                    </Route>
+                  </Switch>
+                </Content>
+              </Layout>
+            </Layout>
+            <Footer>Footer</Footer>
           </Layout>
-        </Layout>
-        <Footer>Footer</Footer>
-      </Layout>
+        </InterfaceContextProvider>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
