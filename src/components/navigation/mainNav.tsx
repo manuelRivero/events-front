5da943e502998a0f298f75ec8f831829d8e7131d
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-05-18 22:29:59 -0300

    [fix] active link in header

--- src/components/navigation/mainNav.tsx
+++ src/components/navigation/mainNav.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {AuthContext} from './../../context/authContext'
 import { Layout, Menu, Typography} from 'antd';
 import {Link} from 'react-router-dom';
 import { UserOutlined } from '@ant-design/icons';
@@ -10,34 +11,34 @@ const { Title } = Typography;
 
 
 
-const mainNav = () => {
+const MainNav = () => {
+  const {userId}= useContext(AuthContext);
     return (
         <Header className="header" style={{display:"flex", padding:" 0 25px"}}>
         <div className="logo" style={{flex: "0 1 175px", display:"flex", alignItems:"center", fontFamily: " Montserrat, sans-serif"}}> 
           <img src={logo} style={{width:"30px"}} alt="Logo" />
           <Title style={{color:"#fff", margin:" 0 10px", fontSize:"18px"}}>Booking App</Title>
         </div>
-        <Menu theme="dark" style={{flex:"auto"}} mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu theme="dark" style={{flex:"auto", display:"flex"}} mode="horizontal" >
           <Menu.Item key="1">
               <Link to="/events">Events</Link>
           </Menu.Item>
           <Menu.Item key="2">
           <Link to="/bookings">Bookings</Link>
           </Menu.Item>
-        </Menu>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          
-          <SubMenu  icon={<UserOutlined />}>
+          { userId ? <SubMenu style={{marginLeft:"auto"}}  icon={<UserOutlined />}>
           <Menu.ItemGroup title="Item 1">
             <Menu.Item key="setting:1">Option 1</Menu.Item>
           </Menu.ItemGroup>
           <Menu.ItemGroup title="Item 2">
             <Menu.Item key="setting:4">Logout</Menu.Item>
           </Menu.ItemGroup>
-        </SubMenu>
+        </SubMenu> : <Menu.Item style={{marginLeft:"auto"}}  key="3">
+          <Link to="/Auth">Login</Link>
+          </Menu.Item> }
         </Menu>
       </Header>
     )
 }
 
-export default mainNav
+export default MainNav
